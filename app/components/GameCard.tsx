3e0a72b8d0571cd808ad233e92c0cdd7e2ac4914
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2024-02-20 20:23:36 +0100

    Displaying platform icons

--- app/components/GameCard.tsx
+++ app/components/GameCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Game } from '../hooks/useGames';
-import { Card, CardBody, Heading } from '@chakra-ui/react';
+import { Card, CardBody, Heading, Text } from '@chakra-ui/react';
 import Image from 'next/image';
+import { platform } from 'os';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
   game: Game;
@@ -15,11 +17,17 @@ const GameCard = ({ game }: Props) => {
         height={400}
         src={game.background_image}
         alt={game.name}
+        style={{
+          width: '100%',
+        }}
       />
       <CardBody>
         <Heading fontSize="2xl" textOverflow={'ellipsis'} noOfLines={1}>
           {game.name}
         </Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
---
+++ app/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Platform } from '../hooks/useGames';
+import { HStack, Icon } from '@chakra-ui/react';
+import {
+    FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from 'react-icons/fa';
+import { BsGlobe } from 'react-icons/bs';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { IconType } from 'react-icons';
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: {[key: string]: IconType} = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    web: BsGlobe,
+    ios: MdPhoneIphone,
+  };
+  return (<HStack my={1}>
+    {platforms.map((platform) => (
+    <Icon key={platform.id} as={iconMap[platform.slug]}  color={'gray.500'}/>
+  ))}
+  </HStack>)
+};
+
+export default PlatformIconList;
