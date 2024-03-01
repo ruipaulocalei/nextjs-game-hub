5ac5237c1ecd17b9c9e3b2496827179ba8c03279
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2024-03-01 05:03:30 +0100

    Improving the layout in Game Detail

--- app/games/[slug]/page.tsx
+++ app/games/[slug]/page.tsx
@@ -3,10 +3,11 @@ import CriticScore from '@/app/components/CriticScore';
 import DefinitionItem from '@/app/components/DefinitionItem';
 import ExpandableText from '@/app/components/ExpandableText';
 import GameAttribute from '@/app/components/GameAttribute';
+import GameScreenshots from '@/app/components/GameScreenshots';
 import GameTrailer from '@/app/components/GameTrailer';
 import NavBar from '@/app/components/NavBar';
 import useGame from '@/app/hooks/useGame';
-import { Box, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
+import { Box, GridItem, Heading, SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import { useParams } from 'next/navigation';
 
 const GameDetail = () => {
@@ -17,10 +18,17 @@ const GameDetail = () => {
   return (
     <Box p={5}>
       <NavBar />
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game?.description_raw}</ExpandableText>
-      <GameAttribute game={game} />
-      <GameTrailer gameId={game.id} />
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <GridItem>
+          <Heading>{game?.name}</Heading>
+          <ExpandableText>{game?.description_raw}</ExpandableText>
+          <GameAttribute game={game} />
+        </GridItem>
+        <GridItem>
+          <GameTrailer gameId={game.id} />
+          <GameScreenshots gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
     </Box>
   );
 };
